5d71436d6a1e1aa19cf56e802da500cef2276f5e
Ohad Ravid <ohad.rv@gmail.com>
2025-05-15 13:17:00 +0300

    Implement converting from JobError to io::Error

--- src/error.rs
+++ src/error.rs
@@ -13,3 +13,36 @@ pub enum JobError {
     #[error("Failed to get info for job")]
     GetInfoFailed(io::Error),
 }
+
+impl From<JobError> for io::Error {
+    fn from(value: JobError) -> Self {
+        match value {
+            JobError::CreateFailed(error)
+            | JobError::AssignFailed(error)
+            | JobError::SetInfoFailed(error)
+            | JobError::GetInfoFailed(error) => error,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn job_error_into_io_error() {
+        fn example() -> Result<(), JobError> {
+            Err(JobError::AssignFailed(io::Error::from_raw_os_error(1)))
+        }
+
+        fn test_example() -> Result<(), io::Error> {
+            let _ = example()?;
+
+            Ok(())
+        }
+
+        let e = test_example().unwrap_err();
+
+        assert_eq!(e.raw_os_error(), Some(1));
+    }
+}
